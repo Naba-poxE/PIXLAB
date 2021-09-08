e629fa187f10abf5e511b074c5310d0f0eebe70b
Naba Expo <49324233+Naba-poxE@users.noreply.github.com>
2021-09-08 13:23:37 +0545

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -115,7 +115,7 @@ class PhotoGallery{
           item.classList.add('mb-3');
           item.innerHTML = `
             <a href="${photo.src.original}" class="fancybox" data-fancybox="gallery1" style="text-decoration: none; color:pink;">
-              <img src="${photo.src.medium}" style="object-fit: cover; width:100%; height:300px; border-top-left-radius: 0.4em; border-top-right-radius: 0.4em;">
+              <img class="work__img" src="${photo.src.medium}" style="object-fit: cover; width:100%; height:300px; border-top-left-radius: 0.4em; border-top-right-radius: 0.4em;">
             </a>
               <h3 style="color: #ffffff82;border-bottom-left-radius: 0.4em; text-align: center;padding: 0.5em;border-bottom-right-radius: 0.4em; background-color: ${photo.avg_color}; cursor: pointer;" onclick="forceDownload('${photo.src.original}', 'nabaexpo_${photo.photographer_id}_pixles.jpeg');">
                   📥 Download 
